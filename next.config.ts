c28716c27cca0545d00abef85347c70acc3b73c7
ggps2002 <155824827+ggps2002@users.noreply.github.com>
2025-07-15 17:55:01 +0530

    disable eslint

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,11 @@ const nextConfig: NextConfig = {
    images: {
     domains: ['images.unsplash.com'], // add any domain you load images from
   },
+    eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
